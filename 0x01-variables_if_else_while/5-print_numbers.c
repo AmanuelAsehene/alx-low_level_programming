4161f48d4f3266cd876a15d2317e2c73fddc03e1
Amanuel Ashene <104828408+AmanuelAsehene@users.noreply.github.com>
2022-07-01 17:39:51 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,29 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
 int i;
 
-for (i = 0; i < 10; i++)
-
-	printf("%d", i);
-
-	printf("\n");
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+		printf("\n");
 
 	return (0);
-
 }
