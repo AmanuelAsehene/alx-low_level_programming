557da02856a61f096f2a75407894fcff47bf5fb0
Amanuel Ashene <104828408+AmanuelAsehene@users.noreply.github.com>
2022-07-01 17:35:53 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
@@ -20,5 +19,5 @@ int main(void)
 		}
 	}
    		printf("\n");
-	return (0);
+	        return (0);
 }
